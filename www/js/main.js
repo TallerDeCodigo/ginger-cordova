57f05cd228617146a356108e0126ce033deada1c
Holkan <luah613@gmail.com>
2016-08-10 17:51:49 -0500

    test camera plugin

--- www/js/main.js
+++ www/js/main.js
@@ -881,25 +881,6 @@
 				console.log(response);
 			});
 		}//END GET DIET
-
-
-/*
-	CAMERA PLUGIN
-*/
-		function setOptions(srcType) {
-		    var options = {
-		        // Some common settings are 20, 50, and 100
-		        quality: 50,
-		        destinationType: Camera.DestinationType.FILE_URI,
-		        // In this app, dynamically set the picture source, Camera or photo gallery
-		        sourceType: srcType,
-		        encodingType: Camera.EncodingType.JPEG,
-		        mediaType: Camera.MediaType.PICTURE,
-		        allowEdit: true,
-		        correctOrientation: true  //Corrects Android orientation quirks
-		    }
-		    return options;
-		}
 	};
 
 /*      _                                       _                        _       
